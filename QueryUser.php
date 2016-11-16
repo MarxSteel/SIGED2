7e65bcd33e98b902e843cb4773fc1335a1daf197
Marx Medeiros <analista.marx@gmail.com>
2016-11-16 21:11:43 -0200

    Revisão

--- QueryUser.php
+++ QueryUser.php
@@ -3,10 +3,6 @@
  $query->execute();
   $row = $query->fetch();
   $NomeUserLogado = $row['Nome'];
-  $permFw = $row['pFw'];				//PERMISSÃO PARA ALTERAR FW
-  $permSup = $row['pSup'];				//PERMISSÃO PARA ALTERAR FW
-  $permRel = $row['pRel'];				//PERMISSÃO PARA GERAR RELATÓRIO
-  $permUsr = $row['pUsr'];				//PERMISSÃO PARA GERAR RELATÓRIO
-  $Reabrir = $row['pReabre'];				//PERMISSÃO PARA GERAR RELATÓRIO
+  $Distrito = $row['Distrito'];				//PERMISSÃO PARA ALTERAR FW
 
 ?>
