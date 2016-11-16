989790307c25f3d2559fbe48b569bc20dee51541
Marx Medeiros <analista.marx@gmail.com>
2016-11-16 21:46:09 -0200

    Adicionando busca dos privilégios do usuário

--- QueryUser.php
+++ QueryUser.php
@@ -3,6 +3,11 @@
  $query->execute();
   $row = $query->fetch();
   $NomeUserLogado = $row['Nome'];
-  $Distrito = $row['Distrito'];				//PERMISSÃO PARA ALTERAR FW
+  $Distrito = $row['Distrito'];								//DISTRITO DO USUÁRIO
+  $PrivClubes = $row['PClubes'];							//Privilégio para visualizar Clubes
+  $PrivAssociado = $row['PAssociados'];						//Privilégios para Acessar Associados
+  $PrivTesouraria = $row['PTesouraria'];					//Privilégio para Acessar Tesouraria
+  $PrivSecretaria = $row['PSecretaria'];					//Privilégio para Acessar Secretaria
+  $PrivANP = $row['PANP'];									//Privilégios ANP
 
 ?>
