09225b0a5f54fc04144d7fde7825408fa23e551c
Marx Medeiros <analista.marx@gmail.com>
2016-11-25 09:26:47 -0200

    Revisão

--- dashboard.php
+++ dashboard.php
@@ -64,13 +64,40 @@ require_once 'QueryUser.php';
   </aside>
 <div class="content-wrapper">
  <section class="content-header">
-  <h1>Página Inicial<small><?php echo $Titulo; ?></small></h1>
+  <h1>Página Inicial
+   <small><?php echo "<strong> Distrito " . $Distrito . "</strong> " . $Titulo; ?></small>
+  </h1>
  </section>
  <section class="content">
   <div class="row">
-
-
- 
+  <?php if ($PrivClubes === "1") { ?>
+   <div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="info-box">
+     <a href="Clubes/dashboard.php" >
+      <span class="info-box-icon btn-primary"><i class="fa fa-industry"></i></span>
+     </a>
+     <div class="info-box-content"><h4>Lista de Clubes</h4></div>
+    </div>
+   </div>
+  <?php } else { } ?>
+   <div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="info-box">
+     <a href="Distrito/Associados.php" >
+      <span class="info-box-icon btn-danger"><i class="fa fa-user"></i></span>
+     </a>
+     <div class="info-box-content"><h4>Lista de Associados</h4></div>
+    </div>
+   </div>
+   <div class="col-md-4 col-sm-6 col-xs-12">
+    <div class="info-box">
+     <a href="Distrito/Tesouraria.php" >
+      <span class="info-box-icon bg-black disabled">
+       <i class="fa fa-dollar"></i>
+      </span>
+     </a>
+     <div class="info-box-content"><h4>Tesouraria</h4></div>
+    </div>
+   </div>
  </section>
 </div><!-- CONTENT-WRAPPER -->
 <?php include_once 'footer.php'; ?>
