4b9654f67898806f6eaa7086df05bd6b1d233dfe
Marx Medeiros <analista.marx@gmail.com>
2017-03-06 12:14:20 -0300

    Revisado

--- Associados/dashboard.php
+++ Associados/dashboard.php
@@ -316,5 +316,31 @@ function formatar(mascara, documento){
     $(".select3").select2();
   });
 </script>
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $(".select4").select2();
+  });
+</script>
+    <script type="text/javascript">
+    $(function(){
+      $('#cod_estados').change(function(){
+        if( $(this).val() ) {
+          $('#cod_cidades').hide();
+          $('.carregando').show();
+          $.getJSON('cidades.ajax.php?search=',{cod_estados: $(this).val(), ajax: 'true'}, function(j){
+            var options = '<option value=""></option>'; 
+            for (var i = 0; i < j.length; i++) {
+              options += '<option value="' + j[i].cod_cidades + '">' + j[i].nome + '</option>';
+            } 
+            $('#cod_cidades').html(options).show();
+            $('.carregando').hide();
+          });
+        } else {
+          $('#cod_cidades').html('<option value="">– Escolha um estado –</option>');
+        }
+      });
+    });
+    </script>
 </body>
 </html>
